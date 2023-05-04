e8f4a8a72a6a1b33300326b0dc2a4b1efc7779b7
Nir Lavi <nirlavi@microsoft.com>
2023-05-04 16:42:28 +0300

    save

--- npmclient/npmclient.py
+++ npmclient/npmclient.py
@@ -2,15 +2,14 @@ import requests
 import json
 from collections import namedtuple
 
-print('Debug: npmclient.py')
 BASE_URL = None
 BASE_PORT = 81
 
 Access_Rule_Client = namedtuple('Access_Rule_Client', ['address', 'directive'])
 
-def get_auth_token(emailAddress: str, password: str):
+def get_auth_token(emailAddress: str, password: str) -> str:
+    global BASE_URL, BASE_PORT
     url = f"http://{BASE_URL}:{BASE_PORT}/api/tokens"
-    print(f'Debug: Url: {url}')
     payload = {"identity": emailAddress ,"secret":password}
 
     response = requests.post(url, json=payload)
@@ -22,7 +21,7 @@ def get_auth_token(emailAddress: str, password: str):
     else:
         print(f"Error getting token. ({response.status_code})")    
 
-def add_access_list(name: str, token: str, *clients: Access_Rule_Client):
+def add_access_list(name: str, token: str, *clients: Access_Rule_Client) -> int:
     payload = { "name" : name,
                 "satisfy_any" : False,
                 "pass_auth" : False,
@@ -35,12 +34,12 @@ def add_access_list(name: str, token: str, *clients: Access_Rule_Client):
         'Authorization' : f'Bearer {token}'
     }
 
-    print(f'debug: request_payload: {payload}')
     response = requests.post(url, json=payload, headers=headers)
     if response.status_code in [200, 201]:
-        response_json = json.loads(response.text)
-        print(f'debug: response_json: {response_json}')
+        response_json = response.json()
         id = response_json["id"]
         print(f'Successfully created new rule (id: {id})')
+        return id
     else:
         print(f"Error adding access list. ({response.status_code})")    
+        return -1
