99ea552b8ca7ee5b0640c7fda83f4dba8518c702
Nir Lavi <nirlavi@microsoft.com>
2023-05-04 11:57:35 +0300

    works

--- add-npm-geofilter.py
+++ add-npm-geofilter.py
@@ -24,13 +24,15 @@ parser.add_argument('--npm-host', help='The npm host / ip to connect to', requir
 parser.add_argument('--npm-port', help='The npm port admin port', required=True)
 parser.add_argument('--npm-email', help='npm admin email address', required=True)
 parser.add_argument('--npm-password', help='npm admin password', required=True)
+parser.add_argument('--npm-accesslist-name', help='The name of the access list', required=True)
 parser.add_argument('--allowed-countries', help='The list of allowed country codes',type=csv_list, required=True)
 parser.add_argument('--ip-list-file', help='The file containing the ip list', required=False)
 
 args = parser.parse_args()
 
-npmclient.BASE_URL = args.npm_host
-npmclient.BASE_PORT = args.npm_port
+npmclient.npmclient.BASE_URL = args.npm_host
+
+npmclient.npmclient.BASE_PORT = args.npm_port
 
 # Get IP file data
 if args.ip_list_file is None:
@@ -41,15 +43,19 @@ else:
 
 #load ip file data
 ip_df = pd.read_csv(ip_file, header=None, names=['start_ip', 'end_ip', 'country_code'])
-filtered_entries = ip_df[(ip_df['country_code'].isin(args.allowed_countries))]
+filtered_entries = (ip_df[(ip_df['country_code'].str.lower().isin([c.lower() for c in args.allowed_countries])) & (ip_df['start_ip'].apply(lambda x: isinstance(ipaddress.ip_address(x), ipaddress.IPv4Address)))])
+print(f'Found {len(filtered_entries)} entries for {args.allowed_countries}')
 
 # create Access_Rule_Client list
 access_rule_list = [npmclient.Access_Rule_Client(address=ip_range_to_cidr(row.start_ip, row.end_ip), directive='allow') for row in filtered_entries.itertuples(index=False)]
 
 #Get auth token
+print('Getting auth token')
+print(f'Debug: npmclient.BASE_URL: {npmclient.BASE_URL}')
 token = npmclient.get_auth_token(args.npm_email, args.npm_password)
 
 #Add access list
-npmclient.add_access_list('geofilter', token, *access_rule_list)
+print('Adding access list')
+npmclient.add_access_list(args.npm_accesslist_name, token, *access_rule_list)
 
 
--- npmclient/npmclient.py
+++ npmclient/npmclient.py
@@ -2,13 +2,15 @@ import requests
 import json
 from collections import namedtuple
 
+print('Debug: npmclient.py')
 BASE_URL = None
 BASE_PORT = 81
 
 Access_Rule_Client = namedtuple('Access_Rule_Client', ['address', 'directive'])
 
 def get_auth_token(emailAddress: str, password: str):
-    url = f"http://{BASE_URL}:{BASE_PORT}/api/login"
+    url = f"http://{BASE_URL}:{BASE_PORT}/api/tokens"
+    print(f'Debug: Url: {url}')
     payload = {"identity": emailAddress ,"secret":password}
 
     response = requests.post(url, json=payload)
@@ -25,7 +27,7 @@ def add_access_list(name: str, token: str, *clients: Access_Rule_Client):
                 "satisfy_any" : False,
                 "pass_auth" : False,
                 "items": [],
-                "clients": clients                 
+                "clients": [{'address': rule.address, 'directive': rule.directive} for rule in clients]                 
             }
     
     url = f"http://{BASE_URL}:{BASE_PORT}/api/nginx/access-lists"
@@ -33,10 +35,12 @@ def add_access_list(name: str, token: str, *clients: Access_Rule_Client):
         'Authorization' : f'Bearer {token}'
     }
 
-    response = requests.post(url, json=payload)
-    if response.status_code == 200:
+    print(f'debug: request_payload: {payload}')
+    response = requests.post(url, json=payload, headers=headers)
+    if response.status_code in [200, 201]:
         response_json = json.loads(response.text)
+        print(f'debug: response_json: {response_json}')
         id = response_json["id"]
         print(f'Successfully created new rule (id: {id})')
     else:
-        print(f"Error getting token. ({response.status_code})")    
+        print(f"Error adding access list. ({response.status_code})")    
